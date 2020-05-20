f3877935e04f351fb4d57363aff2f49e88acc38d
pclavell <55409811+pclavell@users.noreply.github.com>
2020-05-20 11:01:50 +0200

    ola

---
+++ polla/templates/processat.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Mask Detection WebApp</title>
+        <meta name="author" content="hackathon-3"/>
+    </head>
+
+    <body>
+
+        <h1>Processament realitzat!</h1>
+
+        <img src={{ name }} alt="processed_image" style="width:500px;height:600px;">
+ 
+    </body>
+
+</html>
